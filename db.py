0058f23e3ec394650f155c846bf8c18baca52c61
fmatsis02 <115476464+fmatsis02@users.noreply.github.com>
2023-10-30 16:06:39 +0000

    Add files via upload
    more working

--- db.py
+++ db.py
@@ -1,55 +1,37 @@
-# import pandas as pd
-# import pandasql as ps
-# import numpy as np
-# from IPython.display import HTML
-#
-# input = ['lettuce','tomato']
-# df = pd.read_csv("RAW_recipes.csv")
-# sql_query = f"""
-# SELECT *
-# FROM {df}
-# WHERE ingredients LIKE "lettuce"
-# """.format(df)
-# #
-# res_df = ps.sqldf(sql_query, locals())
-# HTML(res_df.to_html(escape=False))
-# print(df)
-# print(res_df)
-
 import pandas as pd
-import sqlite3
-#
-# #read the CSV
-# df = pd.read_csv('RAW_recipes.csv')
-# #connect to a database
-# conn = sqlite3.connect("Any_Database_Name.db") #if the db does not exist, this creates a Any_Database_Name.db file in the current directory
-# #store your table in the database:
-# df.to_sql('df', conn)
-# #read a SQL Query out of your database and into a pandas dataframe
-# sql_query = """
-# SELECT *
-# FROM df
-# WHERE ingredients LIKE "LETTUCE"
-# """
-# df = pd.read_sql(sql_query, conn)
-# print(df)
 import re
-input = ['tomato','lettuce']
+input = ['honey']
 data = pd.read_csv('RAW_recipes.csv')
-
-for i in range(len(input)):
-    reg = '\[(.*, )*(.* )input[i](, .*)*\]'
-    re.findall(reg,data['ingredients'])
-print(data)
-print(re)
 recommend = []
-# for i in range(len(input)):
-#     for j in range(len(data['ingredients'])):
-#         if input[i] in data['ingredients'][j]:
-#             recommend.append(data['id'][j])
-#
-# # print(recommend)
-# # print(type(data['ingredients'][0]))
+i=0
+normal_reg = "\[(.*, )*.*'(, .*)*"+input[i]+"(, .*)*'(, .*)*\]"
+# print(recommend)
+# print(data['ingredients'][0])
+# print(normal_reg)
+# print(type(data['ingredients'][0]))
+# if bool(re.search(normal_reg, str(data['ingredients'][0]))):
+#     print('true')
+# else:
+#     print('false')
+# test = "['winter squash', 'mexican seasoning', 'mixed spice', 'honey', 'butter', 'olive oil', 'salt']"
+# if bool(re.search(normal_reg, test)):
+#     print('true')
+# else:
+#     print('false')
+d = {'name':[], 'id':[], "steps":[], "description":[],"ingredients":[]}
+df = pd.DataFrame(data = d)
+print(df)
+for i in range(len(data['ingredients'][i])):
+    if bool(re.search(normal_reg, data['ingredients'][i])):
+        re.search(normal_reg, data['ingredients'][i])
+        recommend.append(data['id'][i])
+        print(data['ingredients'][i])
+        df.append({'name':data['name'][i]})
+        df.append({'id': data['id'][i]})
+        df.append({'steps': data['steps'][i]})
+        df.append({'description': data['description'][i]})
+        df.append({'ingredients': data['ingredients'][i]})
 
-\[(.*, )*.*\btomato\b(, .*)*\]
-\[(.*, )*(.* )tomato(, .*)*\]
+print(recommend)
+print(df)
+# \[(.*, )*.*tomato(, .*)*egg(, .*)*\]
